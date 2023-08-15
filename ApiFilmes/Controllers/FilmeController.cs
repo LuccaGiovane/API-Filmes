e1eb258d9732464f900c6a081914a06981245583
Lucca Giovane Gomes <lucca.giovane.gomes@uel.br>
2023-08-15 19:08:43 -0300

    Função para consulta dos filmes. Identificador para cada filme. Função para consulta de um filme via LINQ usando o identificador.

--- ApiFilmes/Controllers/FilmeController.cs
+++ ApiFilmes/Controllers/FilmeController.cs
@@ -8,13 +8,27 @@ namespace ApiFilmes.Controllers;
 public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
     public void AdicionaFilme([FromBody] Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
         Console.WriteLine(filme.Titulo);
         Console.WriteLine(filme.Duracao);
 
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaTodosOsFilmes()
+    {
+        return filmes;
+    }
+
+    [HttpGet("{id}")]
+    public Filme? RecuperaFilmePorId(int id)
+    {
+       return filmes.FirstOrDefault(filme => filme.Id == id);
     }
 }
--- ApiFilmes/Models/Filme.cs
+++ ApiFilmes/Models/Filme.cs
@@ -4,6 +4,8 @@ namespace ApiFilmes.Models
 {
     public class Filme
     {
+        public int Id { get; set; }
+
         [Required(ErrorMessage ="Título Obrigatório!")]
         [MaxLength(50, ErrorMessage = "O Título não deve exceder 50 caracteres!")]
         public string Titulo { get; set; }
@@ -15,5 +17,6 @@ namespace ApiFilmes.Models
         [Required(ErrorMessage = "Duração Obrigatória!")]
         [Range(70, 600, ErrorMessage ="A Duração deve ser entre 70 e 600 minutos!")]
         public int Duracao { get; set; }
+        
     }
 }
